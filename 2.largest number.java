b16e1aad9023c972a854be9cc618527187b24f64
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-05-31 20:37:47 +0530

    Create 2.largest number.java

---
+++ 2.largest number.java
@@ -0,0 +1,27 @@
+public class Main{
+  
+  public static int getlargest(int list[]){
+
+    int largest = Integer.MIN_VALUE;
+
+    for (int i=0; i< list.length ; i++ ){
+
+      if ( largest < list[i]){
+        largest = list[i];
+      }
+      
+    }
+
+    return largest;
+    
+  }
+  
+  public static void main(String args[]){
+
+    int list[] = {1,2,3,4,5,6,7};
+    int result = getlargest(list);
+    System.out.println("Largest Number :" + result);
+
+    
+  }
+}
