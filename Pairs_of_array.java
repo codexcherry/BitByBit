e9f47398e865ea9bb47e0750478c8a1d5f83144d
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-07-29 21:35:47 +0530

    Create Pairs_of_array.java

---
+++ Pairs_of_array.java
@@ -0,0 +1,22 @@
+package JAVA;
+
+public class Pairs_of_array {
+
+  // arraypair fuction
+	public static void subarr(int numbers[]) {
+		for (int i=0;i<numbers.length;i++) {
+			for (int j=i;j<numbers.length;j++) {
+				System.out.print("(" + numbers[i] + "," + numbers[j] + ")");
+			}
+			System.out.println();
+		}
+	}
+
+  // Main Function
+	public static void main(String[] args) {
+		int numbers[] = {1, 2, 3, 4, 5};
+		subarr(numbers);
+    
+	}
+
+}
