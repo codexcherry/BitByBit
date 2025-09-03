42d23e4104319508006bebc9539c744cfa89039a
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-09-03 13:19:47 +0530

    Update 5-Reverse-Number.java

--- Arrays/5-Reverse-Number.java
+++ Arrays/5-Reverse-Number.java
@@ -1,6 +1,6 @@
-package JAVA;
+// package JAVA;
 
-public class Reverse_Number {
+// public class Reverse_Number {
 	
 	
 	public static void reverse(int data[]) {
@@ -16,15 +16,15 @@ public class Reverse_Number {
 		}
 	}
 
-	public static void main(String[] args) {
+// 	public static void main(String[] args) {
     
-		int data[] = {1,2,3,4,5,6,7,8};
-		reverse(data);
-		for (int i=0; i<data.length ; i++) {
-			System.out.print(data[i] + " " );
+// 		int data[] = {1,2,3,4,5,6,7,8};
+// 		reverse(data);
+// 		for (int i=0; i<data.length ; i++) {
+// 			System.out.print(data[i] + " " );
       
-		}
+// 		}
 
-	}
+// 	}
 
-}
+// }
