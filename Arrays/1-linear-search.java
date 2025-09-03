c948c8fd0185737c6bf078684f166f9a8c8db91f
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-09-03 13:15:06 +0530

    Update and rename 1 . Linear Search - easy to Arrays/1-linear-search.java

---
+++ Arrays/1-linear-search.java
@@ -0,0 +1,32 @@
+// public class Main{
+
+  public static int linearsearch(int list[],int key){
+    
+    for (int i =0 ; i < list.length; i++){
+      if ( list[i] == key ){
+        return i+1;
+      }
+    }
+    return -1;
+  }
+
+
+
+//   public static void main(String args[]){
+
+//     int list[] = {1,2,3,4,5,6,7};
+    
+//     int key=4;
+
+//     int result = linearsearch(list,key);
+//     if ( result == -1){
+//       System.out.println("Key not Found");
+//     }
+//     else {
+//       System.out.println("Key Found :" + result );
+      
+//     }
+    
+//   }
+  
+// }
