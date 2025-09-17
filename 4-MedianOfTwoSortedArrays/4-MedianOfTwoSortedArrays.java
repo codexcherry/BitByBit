77f34f7fcac3bdde7ad835c3de1b4ca9bf8a8f33
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-09-17 21:29:04 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.java
@@ -0,0 +1,36 @@
+// Last updated: 9/17/2025, 9:29:04 PM
+import java.lang.Math;
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = ((x + y + 1) / 2) - partitionX;
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return (double)(Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return (double) Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+        throw new IllegalArgumentException("The input arrays are not sorted.");
+    }
+}
