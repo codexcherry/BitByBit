eb820e8f9114881019ce53ba14e3c01ef457b114
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-09-17 21:28:41 +0530

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ 167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,34 @@
+// Last updated: 9/17/2025, 9:28:41 PM
+class Solution {
+    public int[] twoSum(int[] nums, int tr) {
+
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         int sum = nums[i] + nums[j];
+        //         if(sum == tr){
+        //             return new int[]{i+1,j+1};
+        //         }
+        //     }
+        // }
+
+        // return new int[]{};
+
+
+        int low=0;
+        int high = nums.length-1;
+
+        while(low<high){
+            int sum = nums[low] + nums[high];
+            if(sum==tr){
+                return new int[]{low+1,high+1};
+            }
+            else if(sum > tr){
+                high--;
+            }
+            else{
+                low++;
+            }
+        }
+        return new int[]{};
+    }
+}
