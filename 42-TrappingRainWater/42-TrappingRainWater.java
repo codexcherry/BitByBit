52b9875b4e7b8921b844a1ab42a016e33a2cb7ab
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-09-17 21:28:59 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,34 @@
+// Last updated: 9/17/2025, 9:28:59 PM
+class Solution {
+    public int trap(int[] height) {
+
+
+        int n=height.length;
+
+        //calaculate the leftmax
+        int leftmax[] = new int[n];
+        leftmax[0] = height[0];
+        for(int i=1;i<n;i++){
+            leftmax[i] = Math.max(height[i],leftmax[i-1]);
+        }
+        //calaculate the rightmax
+        int rightmax[] = new int[n];
+        rightmax[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i] = Math.max(height[i],rightmax[i+1]);
+        }
+
+        //loop
+        //clalculate waterlevel
+        //calculate traped water
+
+        int trappedwater=0;
+        for(int i=0;i<n;i++){
+            int waterlevel = Math.min(leftmax[i],rightmax[i]);
+            trappedwater += waterlevel - height[i];
+        }
+        
+
+        return trappedwater;
+    }
+}
