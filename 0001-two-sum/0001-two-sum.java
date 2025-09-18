bc7e76b5071b5fd195b1ab23b43d0994d5bde338
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-09-18 09:06:23 +0530

    Time: 4 ms (57.59%), Space: 44.9 MB (80.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
 
+        // Using hashmap 
         Map<Integer,Integer> mapp = new HashMap<>();
-      
+
         for(int i=0;i<nums.length;i++){
-            int comp = target - nums[i];
-            if(mapp.containsKey(comp)){
-                return new int[]{mapp.get(comp),i};
+            int remaning = target - nums[i];
+            if(mapp.containsKey(remaning)){
+                return new int[]{mapp.get(remaning),i};
             }
             mapp.put(nums[i],i);
         }
 
         return new int[]{};
-
-
     }
 }
