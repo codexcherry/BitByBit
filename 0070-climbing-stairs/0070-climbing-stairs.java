f42c19aaa74c4b5b3edd9aa831dcfbd09a458a22
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-10-06 21:17:41 +0530

    Time: 0 ms (100%), Space: 40.3 MB (67.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,56 +1,20 @@
 class Solution {
 
+    public static int dpsolution(int n, int[] dp) {
+        if (n == 1) return dp[n] = 1;
+        if (n == 2) return dp[n] = 2;
 
-    public static int dpsolution(int n,int[] dp){
+        if (dp[n] != -1) return dp[n];
 
-        if(n == 1){
-            dp[n] = 1;
-            return 1;
-        }
-        if(n == 2){
-            dp[n] = 2;
-            return 2;
-        }
+        int left = dpsolution(n - 1, dp);
+        int right = dpsolution(n - 2, dp);
 
-        int left,right;
-        if(dp[n-1] != -1){
-            left = dp[n-1];
-        }else{
-            left = dpsolution(n-1,dp);
-        }
-
-        if(dp[n-2] != -1){
-            right = dp[n-2];
-        }else{
-            right = dpsolution(n-2,dp);
-        }
-
-        dp[n] = left + right;
-        return dp[n];
-
+        return dp[n] = left + right;
     }
 
-
-
-
-
-
     public int climbStairs(int n) {
-        // if(n == 1){return 1;}
-        // if(n == 2){return 2;}
-        // int left = climbStairs(n-1);
-        // int right = climbStairs(n-2);
-
-        // return left+right;
-
-        int dp[] = new int[n+1];
-
-        for(int i=0;i<dp.length;i++){
-            dp[i] = -1;
-        }
-        int dpresult = dpsolution(n,dp);
-
-        return dpresult;
-
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) dp[i] = -1;
+        return dpsolution(n, dp);
     }
 }
