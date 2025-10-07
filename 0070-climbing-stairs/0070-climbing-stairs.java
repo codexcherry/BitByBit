d7837ea5cfef6ccea97d4dea7c7f3a29ba9cb4cb
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-10-07 10:33:10 +0530

    Time: 0 ms (100%), Space: 40.8 MB (19.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -12,9 +12,25 @@ class Solution {
         return dp[n] = left + right;
     }
 
+    public static int tabluter(int n){
+         if (n == 1) return 1;
+        if (n == 2) return 2;
+        int prev2 = 1;
+        int prev = 2; 
+        for(int i=2;i<n;i++){
+            int curr = prev2+prev;
+            prev2=prev;
+            prev=curr;
+        }
+
+        return prev;
+    }
+
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        for (int i = 0; i <= n; i++) dp[i] = -1;
-        return dpsolution(n, dp);
+        // int[] dp = new int[n + 1];
+        // for (int i = 0; i <= n; i++){ dp[i] = -1 };
+        // return dpsolution(n, dp);
+
+        return tabluter(n);
     }
 }
