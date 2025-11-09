68b29b664a5d4b418446ab0e8e359f1f5c5cd2c3
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-11-09 19:23:48 +0530

    Time: 1 ms (99.76%), Space: 55.6 MB (5.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,52 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
+
+        // ----------------------------------------------------
+
+        //           Brute Force
+
+        // ----------------------------------------------------
+
         int n = nums.length;
 
-        for(int i=0;i<nums.length;i++){
-            int count = 0 ;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
+        // for(int i=0;i<nums.length;i++){
+        //     int count = 0 ;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count > n/2){
+        //         return nums[i];
+        //     }
+        // }
+
+        // return -1;
+
+        // ----------------------------------------------------
+
+        //           Optimal
+
+        // ----------------------------------------------------
+
+        int count = 0;
+        int element = -1;
+
+        for(int i=0;i<n;i++){
+            if(count == 0){
+                ++count;
+                element = nums[i];
             }
-            if(count > n/2){
-                return nums[i];
+            else if (nums[i] == element){
+                count++;
+            }
+            else{
+                count--;
             }
         }
 
-        return -1;
+        return element;
         
     }
 }
