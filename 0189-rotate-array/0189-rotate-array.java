e22edb1dacceaf89308df466b8290bf185a36f89
NITHIN V S <110377337+codexcherry@users.noreply.github.com>
2025-11-09 19:38:56 +0530

    Time: 0 ms (100%), Space: 61.2 MB (6.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,52 @@
 class Solution {
 
-    public static void reverse(int[] arr,int left,int right){
-        while(left < right){
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--; 
+
+    public static void reverse(int[] nums,int start,int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 
 
 
+
+
     public void rotate(int[] nums, int k) {
+
+        // ----------------------------------------------------
+
+        //           Brute Force
+
+        // ----------------------------------------------------
+
         int n = nums.length;
         k = k % n;
 
-        reverse(nums,0,nums.length-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,nums.length-1);
+        // for(int j=0;j<k;j++){
+        //     int temp = nums[n-1];
+        //     for(int i=n-1;i>0;i--){
+        //         nums[i] = nums[i-1];
+        //     }
+        //     nums[0] = temp;
+        // }
         
+
+        // ----------------------------------------------------
+
+        //           optimal
+
+        // ----------------------------------------------------
+
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+
+
+
+
     }
 }
